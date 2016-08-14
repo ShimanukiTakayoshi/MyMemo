1c222204f468a65db16ced48ecc0bf1f775efe98
shimanuki takayoshi <t-shima@db3.so-net.ne.jp>
2016-08-14 17:12:38 +0900

    １５まで完了

--- MyMemoX/Form1.Designer.vb
+++ MyMemoX/Form1.Designer.vb
@@ -22,9 +22,109 @@ Partial Class Form1
   'コード エディターを使って変更しないでください。
   <System.Diagnostics.DebuggerStepThrough()> _
   Private Sub InitializeComponent()
-    components = New System.ComponentModel.Container()
+    Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+    Me.MenuItemFile = New System.Windows.Forms.ToolStripMenuItem()
+    Me.MenuItemExit = New System.Windows.Forms.ToolStripMenuItem()
+    Me.TextBoxMain = New System.Windows.Forms.TextBox()
+    Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+    Me.MenuItemFileOpen = New System.Windows.Forms.ToolStripMenuItem()
+    Me.MenuItemSave = New System.Windows.Forms.ToolStripMenuItem()
+    Me.MenuItemFileSaveAs = New System.Windows.Forms.ToolStripMenuItem()
+    Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+    Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+    Me.MenuStrip1.SuspendLayout()
+    Me.SuspendLayout()
+    '
+    'MenuStrip1
+    '
+    Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuItemFile})
+    Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+    Me.MenuStrip1.Name = "MenuStrip1"
+    Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+    Me.MenuStrip1.TabIndex = 0
+    Me.MenuStrip1.Text = "MenuStrip1"
+    '
+    'MenuItemFile
+    '
+    Me.MenuItemFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.MenuItemFileOpen, Me.MenuItemSave, Me.MenuItemFileSaveAs, Me.ToolStripSeparator1, Me.MenuItemExit})
+    Me.MenuItemFile.Name = "MenuItemFile"
+    Me.MenuItemFile.Size = New System.Drawing.Size(66, 20)
+    Me.MenuItemFile.Text = "ファイル(&F)"
+    '
+    'MenuItemExit
+    '
+    Me.MenuItemExit.Name = "MenuItemExit"
+    Me.MenuItemExit.Size = New System.Drawing.Size(186, 22)
+    Me.MenuItemExit.Text = "終了(&X)"
+    '
+    'TextBoxMain
+    '
+    Me.TextBoxMain.Location = New System.Drawing.Point(42, 38)
+    Me.TextBoxMain.Name = "TextBoxMain"
+    Me.TextBoxMain.Size = New System.Drawing.Size(100, 19)
+    Me.TextBoxMain.TabIndex = 1
+    '
+    'ToolStripMenuItem1
+    '
+    Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+    Me.ToolStripMenuItem1.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.N), System.Windows.Forms.Keys)
+    Me.ToolStripMenuItem1.Size = New System.Drawing.Size(186, 22)
+    Me.ToolStripMenuItem1.Text = "新規(&N)"
+    '
+    'MenuItemFileOpen
+    '
+    Me.MenuItemFileOpen.Name = "MenuItemFileOpen"
+    Me.MenuItemFileOpen.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+    Me.MenuItemFileOpen.Size = New System.Drawing.Size(186, 22)
+    Me.MenuItemFileOpen.Text = "開く(&O)..."
+    '
+    'MenuItemSave
+    '
+    Me.MenuItemSave.Name = "MenuItemSave"
+    Me.MenuItemSave.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+    Me.MenuItemSave.Size = New System.Drawing.Size(186, 22)
+    Me.MenuItemSave.Text = "上書き保存(&S)"
+    '
+    'MenuItemFileSaveAs
+    '
+    Me.MenuItemFileSaveAs.Name = "MenuItemFileSaveAs"
+    Me.MenuItemFileSaveAs.Size = New System.Drawing.Size(186, 22)
+    Me.MenuItemFileSaveAs.Text = "名前を付けて保存(&A)..."
+    '
+    'ToolStripSeparator1
+    '
+    Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+    Me.ToolStripSeparator1.Size = New System.Drawing.Size(183, 6)
+    '
+    'OpenFileDialog1
+    '
+    Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
     Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(284, 261)
+    Me.Controls.Add(Me.TextBoxMain)
+    Me.Controls.Add(Me.MenuStrip1)
+    Me.MainMenuStrip = Me.MenuStrip1
+    Me.Name = "Form1"
     Me.Text = "Form1"
+    Me.MenuStrip1.ResumeLayout(False)
+    Me.MenuStrip1.PerformLayout()
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
   End Sub
 
+  Friend WithEvents MenuStrip1 As MenuStrip
+  Friend WithEvents MenuItemFile As ToolStripMenuItem
+  Friend WithEvents MenuItemExit As ToolStripMenuItem
+  Friend WithEvents TextBoxMain As TextBox
+  Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+  Friend WithEvents MenuItemFileOpen As ToolStripMenuItem
+  Friend WithEvents MenuItemSave As ToolStripMenuItem
+  Friend WithEvents MenuItemFileSaveAs As ToolStripMenuItem
+  Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+  Friend WithEvents OpenFileDialog1 As OpenFileDialog
 End Class
--- MyMemoX/Form1.vb
+++ MyMemoX/Form1.vb
@@ -1,3 +1,25 @@
 ﻿Public Class Form1
+  Const ApplicationName = "MyMemo"
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Me.Text = ApplicationName
+    TextBoxMain.Multiline = True
+    TextBoxMain.ScrollBars = ScrollBars.Vertical
+    TextBoxMain.Dock = DockStyle.Fill
+  End Sub
+
+  Private Sub MenuItemExit_Click(sender As Object, e As EventArgs) Handles MenuItemExit.Click
+    Me.Close()
+  End Sub
+
+  Private Sub MenuItemFileOpen_Click(sender As Object, e As EventArgs) Handles MenuItemFileOpen.Click
+    OpenFileDialog1.FileName = ""
+    OpenFileDialog1.ShowDialog()
+    loadfile(OpenFileDialog1.FileName)
+  End Sub
+
+  Private Sub LoadFile(value As String)
+    TextBoxMain.Text = System.IO.File.ReadAllText(value, System.Text.Encoding.GetEncoding("Shift_JIS"))
+  End Sub
 
 End Class
